386daac7eee02b1b8e371b80ca6d5df31b7b9562
KristinaBorisova <kkristinaborisova@gmail.com>
2017-01-26 09:06:05 +0200

    public void run() PannelLayout2

--- ReDesigned_Project_Translator/src/MainScreen.java
+++ ReDesigned_Project_Translator/src/MainScreen.java
@@ -2,17 +2,14 @@ import java.awt.EventQueue;
 
 public class MainScreen extends PannelLayout2 {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PanelLayout window = new PanelLayout();
+					PannelLayout2 window = new PannelLayout2();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-	}
-
-}
