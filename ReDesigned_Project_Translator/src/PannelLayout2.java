aef3c558fd499a0a98f3c8f7c7daf1a8b91f068c
KristinaBorisova <kkristinaborisova@gmail.com>
2017-01-28 05:03:44 +0200

    reference to another class "Add words Screen" for button add words

--- ReDesigned_Project_Translator/src/PannelLayout2.java
+++ ReDesigned_Project_Translator/src/PannelLayout2.java
@@ -18,19 +18,25 @@ import javax.swing.JPopupMenu;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.event.AncestorListener;
 import javax.swing.event.AncestorEvent;
 import java.awt.Font;
 import java.awt.Window.Type;
+import java.awt.TextField;
 
-public class PannelLayout2 extends TranslatorTextFilter {
+public class PannelLayout2 extends AddWords {
 
-	static JFrame frame;
+	protected static final int j = 0;
+	public JFrame frmTranslator = new JFrame("Translator");
 	private JTextField outputTextField;
 	public JButton btnClearAll = new JButton("Clear All");
-	protected static JTextField userInputTextField;
 	public JButton btnEnterNewWord = new JButton("Enter word");
 	public JButton btnTranslate = new JButton("Translate");
 
@@ -38,6 +44,7 @@ public class PannelLayout2 extends TranslatorTextFilter {
 	public JMenuItem mntmCut_1 = new JMenuItem("Cut");
 	public JMenuItem mntmPasteText_1 = new JMenuItem("Paste Text");
 	public JMenuItem mntmCountWords = new JMenuItem("Count words");
+	private JMenuBar menuBar;
 
 	// String iconPath = "C:\Desktop\Final Project\icon.png";
 	// Image icon = new
@@ -51,18 +58,15 @@ public class PannelLayout2 extends TranslatorTextFilter {
 	}
 
 	private void menuItemsActions() {
-		// Add Methods
-		// mnEdit.addActionListener(new ActionListener() { //Menu Bar
-		// public void actionPerformed(ActionEvent arg0) {
-		// mnEdit.show();
-		// }
-		// });
+		menuBar.add(mntmCopyText);
+
 		mntmCopyText.addActionListener(new ActionListener() { // Button Copy
 			public void actionPerformed(ActionEvent e) {
 				userInputTextField.copy();
 				outputTextField.copy();
 			}
 		});
+		menuBar.add(mntmCut_1);
 
 		mntmCut_1.addActionListener(new ActionListener() { // Button Cut
 			public void actionPerformed(Action e) {
@@ -75,6 +79,7 @@ public class PannelLayout2 extends TranslatorTextFilter {
 
 			}
 		});
+		menuBar.add(mntmPasteText_1);
 
 		mntmPasteText_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -83,6 +88,7 @@ public class PannelLayout2 extends TranslatorTextFilter {
 				outputTextField.paste();
 			}
 		});
+		menuBar.add(mntmCountWords);
 
 		mntmCountWords.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -97,14 +103,18 @@ public class PannelLayout2 extends TranslatorTextFilter {
 
 				try {
 
-					for ( i = 0; i <= count - i; i++) {
+					for (i = 0; i <= count - i; i++) {
 						if (count >= 1) {
 
 							String tempString = userTextInput.substring(0, userTextInput.indexOf(" "));
 
 							if (userTextInput.contains(" ") && (!userTextInput.isEmpty())) {
 								userTextInput = userTextInput.substring(userTextInput.indexOf(" ") + 1,
-										userTextInput.length()); // get substring between " " and lenght ;
+										userTextInput.length()); // get
+																	// substring
+																	// between "
+																	// " and
+																	// lenght ;
 							} else {
 								if (!userTextInput.isEmpty()) {
 									JOptionPane.showMessageDialog(null, "Empty input"); // output
@@ -133,136 +143,138 @@ public class PannelLayout2 extends TranslatorTextFilter {
 		btnClearAll.setFont(new Font("Verdana", Font.PLAIN, 16));
 		btnClearAll.setForeground(new Color(139, 0, 0));
 		btnClearAll.setBackground(new Color(255, 255, 255));
-		btnClearAll.addActionListener(new ActionListener() { // "Clear All" Button Action
+		btnClearAll.addActionListener(new ActionListener() { // "Clear All"
+																// Button Action
 			public void actionPerformed(ActionEvent arg0) {
 				clearAll();
 			}
 		});
-		
+
+		btnTranslate.setFont(new Font("Verdana", Font.PLAIN, 20));
+		btnTranslate.setBackground(new Color(255, 255, 255));
 
 		btnTranslate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				outputTextField.setText(translatedInput(userInputTextField.getText()));
+				translateInput();
 
 			}
 
 		});
-	}
-		private void translateInput() {
-		btnTranslate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				outputTextField.setText(translatedInput(userInputTextField.getText()));
-			}
-
-				private String translatedInput(String userInput) {
-				String untranslatedText = userInput;
-				int lineOfWord = 1;
-				
-				File allBulgarianWords = new File ("C:\\Desktop\\Final Project\\Database\\bulgarianWords.txt");
-				File allEnglishWords = new File ("C:\\Desktop\\Final Project\\Database\\englishWords.txt");
-				
-				checkForLanguage(untranslatedText);
-				
-				ArrayList<String> userInputUntranslatedText = new ArrayList<String>(Arrays.asList(untranslatedText.split(" ")));
-				
-				String myCurrentWord = null;
-				String myTranslatedWord = null;
-				
-				ArrayList<String> userInputTranslatedText = new ArrayList<String>();
-				
-				Scanner readAllbgInputWords = new Scanner("bulgarianWords.txt");
-				Scanner readenTranslatedWords = new Scanner("englishWords.txt");
-				
-		
-				
-				while (userInputUntranslatedText.equals(null)) {
-								while (userInputUntranslatedText.equals(null)) {
-					while(readAllbgInputWords.hasNextLine()){ 
-						lineOfWord++;
-						//myCurrentWord = readAllbgInputWords.hasNextLine();
-						
-						if(userInputUntranslatedText.equals(myCurrentWord)) {
-							outputTextField.setText(myCurrentWord);
-							break;
-						}
-					}
-				}
-				return myCurrentWord ;
-				
-			}
-					}
-				}
-				return null;
-			}
-
-			private void checkForLanguage(String anyText) {
-				char [] anyTextToCharArray = anyText.toCharArray();
-				char[] bulgarianAlphabet = { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
-						'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'щ', 'ь', 'ю', 'я' };
-				for (int i = 0; i < anyText.length();) {
-					for (int k = 0; k < bulgarianAlphabet.length; k++) {
-						if (anyTextToCharArray[i] == (bulgarianAlphabet[k])) {
-						File inputWord = new File("C:\\Desktop\\Final Project\\Database\\bulgarianWords.txt");
-						File translatedWord = new File("C:\\Desktop\\Final Project\\Database\\englishWords.txt");
-						}
-					}
-
-					break;
-				}
-				
-				
-				
-			}
-
-		});
-	}
-
 		btnEnterNewWord.setFont(new Font("Verdana", Font.PLAIN, 16));
 		btnEnterNewWord.setForeground(new Color(47, 79, 79));
 		btnEnterNewWord.setBackground(new Color(255, 255, 255));
 
 		btnEnterNewWord.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				addNewWord(); // Add code to Method
-
-				
+				AddWordsScreen visualise = new AddWordsScreen(); // Add code to
+																	// Method
+				visualise.main(null);
 			}
 		});
 
 	} // End Of method
 
 	private void clearAll() {
-		userInputTextField.setText("");
 		outputTextField.setText("");
+		userInputTextField.setText("");
 	}
 
-	
-	private void addNewWord() {
+	private void translateInput() {
+		btnTranslate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				outputTextField.setText(translatedInput(userInputTextField.getText()));
+
+				}
+
+			});
+		}
 		
-		 if( userInputTextField.getText().split(" ").length == 1 || outputTextField.getText().split(" ").length == 1 ) {
-		 importToFileEnglishWord(userInputTextField.getText());
-		 importToFileBulgarianWord(outputTextField.getText());
-		 }
-		  else {
-		 JOptionPane.showMessageDialog(null, "Error,try again");
-		 }
-	
-	}
+
+					private String translatedInput(String userInput) {
+					String untranslatedText = userInput;
+					int lineOfWord = 1;
+					
+					File allBulgarianWords = new File ("C:\\Desktop\\Final Project\\Database\\bulgarianWords.txt");
+					File allEnglishWords = new File ("C:\\Desktop\\Final Project\\Database\\englishWords.txt");
+					
+					checkForLanguage(untranslatedText);
+					
+					ArrayList<String> userInputUntranslatedText = new ArrayList<String>(Arrays.asList(untranslatedText.split(" ")));
+					
+					String myCurrentWord = null;
+					String myTranslatedWord = null;
+					
+					ArrayList<String> userInputTranslatedText = new ArrayList<String>();
+					
+//					Scanner readAllbgInputWords = new Scanner("bulgarianWords.txt"); doesn't work
+//					Scanner readenTranslatedWords = new Scanner("englishWords.txt"); can't find files
+					
+			
+					
+					while (userInputUntranslatedText.equals(null)) {
+									
+						while(allBulgarianWords.hasNextLine()){ 
+							lineOfWord++;
+							//myCurrentWord = readAllbgInputWords.hasNextLine();
+							
+							if(userInputUntranslatedText.equals(myCurrentWord)) {
+								outputTextField.setText(myCurrentWord);
+								break;
+							}
+						}
+					}
+					return myCurrentWord ;
+					
+				}
+					
+					
+					
+				
+				private void checkForLanguage(String anyText) {
+					char [] anyTextToCharArray = anyText.toCharArray();
+					char[] bulgarianAlphabet = { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+							'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'щ', 'ь', 'ю', 'я' };
+					for (int i = 0; i < anyText.length();) {
+						for (int k = 0; k < bulgarianAlphabet.length; k++) {
+							if (anyTextToCharArray[i] == (bulgarianAlphabet[k])) {
+							File bulgarianWords = new File("C:\\Desktop\\Final Project\\Database\\bulgarianWords.txt");
+							File EnglishTranslationWords = new File("C:\\Desktop\\Final Project\\Database\\englishWords.txt");
+							}
+						}
+
+						break;
+					}
+				}
+		
+	// private void addNewWord() {
+	// addBulgarianWord(String anyText);
+	// String gosho = "cat";
+	// TranslatorTextFilter textFilter = new TranslatorTextFilter();
+	// textFilter.importToFileEnglishWord(gosho);
+	// public void actionPerformed(ActionEvent e) {
+	// if (userInputTextField.getText().split(" ").length == 1 ||
+	// outputTextField.getText().split(" ").length == 1) {
+	// importToFileEnglishWord(userInputTextField.getText());
+	// importToFileBulgarianWord(outputTextField.getText());
+	// } else {
+	// JOptionPane.showMessageDialog(null, "Error,try again");
+	// }
+	//
+	// }
 
 	private void initializeElements() {
-		frame = new JFrame("Translator");
-		frame.setType(Type.UTILITY);
-		frame.setFont(new Font("Verdana", Font.BOLD, 19));
-		frame.setBackground(new Color(240, 248, 255));
-		frame.getContentPane().setForeground(SystemColor.activeCaption);
-		frame.setBounds(400, 400, 555, 470);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().setBackground(new Color(230, 230, 250));
+
+		frmTranslator.setFont(new Font("Verdana", Font.BOLD, 19));
+		frmTranslator.setBackground(new Color(240, 248, 255));
+		frmTranslator.getContentPane().setForeground(SystemColor.activeCaption);
+		frmTranslator.setBounds(400, 400, 555, 470);
+		frmTranslator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTranslator.getContentPane().setLayout(null);
+		frmTranslator.getContentPane().setBackground(new Color(230, 230, 250));
 
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(12, 230, 505, 139);
-		frame.getContentPane().add(panel_1);
+		frmTranslator.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 
 		outputTextField = new JTextField();
@@ -272,41 +284,29 @@ public class PannelLayout2 extends TranslatorTextFilter {
 		outputTextField.setColumns(10);
 
 		btnTranslate.setBounds(195, 167, 143, 55); // Translate Button
-		frame.getContentPane().add(btnTranslate);
+		frmTranslator.getContentPane().add(btnTranslate);
 		btnTranslate.setForeground(new Color(0, 0, 139));
 		btnClearAll.setBounds(50, 172, 133, 45);
 
-		frame.getContentPane().add(btnClearAll);
+		frmTranslator.getContentPane().add(btnClearAll);
 
 		btnEnterNewWord.setBounds(350, 172, 133, 45); // Enter New Word Button
-		frame.getContentPane().add(btnEnterNewWord);
-		
-		btnTranslate.setFont(new Font("Verdana", Font.PLAIN, 20));
-		btnTranslate.setBackground(new Color(255, 255, 255));
+		frmTranslator.getContentPane().add(btnEnterNewWord);
 
 		JPanel panel = new JPanel();
 		panel.setBounds(12, 19, 505, 140);
-		frame.getContentPane().add(panel);
+		frmTranslator.getContentPane().add(panel);
 		panel.setLayout(null);
 
-		userInputTextField = new JTextField();
-		userInputTextField.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		userInputTextField.setBounds(12, 16, 481, 111);
+		TextField userInputTextField = new TextField();
+		userInputTextField.setBounds(10, 10, 485, 120);
 		panel.add(userInputTextField);
-		userInputTextField.setColumns(10);
 
-		JMenuBar menuBar = new JMenuBar();
+		menuBar = new JMenuBar();
 		menuBar.setBackground(new Color(255, 255, 255));
-		frame.setJMenuBar(menuBar);
+		frmTranslator.setJMenuBar(menuBar);
 
 		JMenu mnEdit = new JMenu("Edit");// Create Menu
 		menuBar.add(mnEdit);
-
-		mnEdit.add(mntmCopyText);
-
-		mnEdit.add(mntmCut_1); // Add to menu
-		mnEdit.add(mntmPasteText_1);
-		mnEdit.add(mntmCountWords); // Add to menu
 	}
-
 }
